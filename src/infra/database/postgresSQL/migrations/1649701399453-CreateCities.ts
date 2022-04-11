8c510671fa5dd7fff12fea36b600c009475189b1
Caroll <carolinne.silva888@gmail.com>
2022-04-11 18:30:40 -0300

    feat: create migration city

---
+++ src/infra/database/postgresSQL/migrations/1649701399453-CreateCities.ts
@@ -0,0 +1,32 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+// eslint-disable-next-line import/prefer-default-export
+export class CreateCities1649701399453 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'city',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'state',
+            type: 'varchar',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('city');
+  }
+}
