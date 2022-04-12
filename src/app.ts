eccac144e3b270043bd0156f8eb2ff417246ffff
Caroll <carolinne.silva888@gmail.com>
2022-04-12 19:07:59 -0300

    fix: remove import unused

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/extensions */
 import 'reflect-metadata';
-import express, { Router } from 'express';
+import express from 'express';
 import 'pg';
 // eslint-disable-next-line import/extensions
 import './infra/database/postgresSQL';
