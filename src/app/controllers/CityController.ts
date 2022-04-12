d9412b21efa24fed97904228824a00976b5ec2b4
Caroll <carolinne.silva888@gmail.com>
2022-04-12 19:02:41 -0300

    ref: controller

--- src/app/controllers/CityController.ts
+++ src/app/controllers/CityController.ts
@@ -3,10 +3,10 @@ import { Request, Response } from 'express';
 import { CityService } from '../service/CityService';
 
 export class CityController {
-  async handle(req: Request, res: Response) {
+  async createCity(req: Request, res: Response) {
     const service = new CityService();
 
-    const result = await service.createCity(req.body);
+    const result = await service.create(req.body);
 
     if (result instanceof Error) {
       return res.status(400).json(result.message);
