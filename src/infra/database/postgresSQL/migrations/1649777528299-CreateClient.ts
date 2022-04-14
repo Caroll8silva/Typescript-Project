0ad7c71d183ebd160006208ab1e2b35e2edd269f
Caroll <carolinne.silva888@gmail.com>
2022-04-14 11:54:17 -0300

    ref: change tipe date to varchar

--- src/infra/database/postgresSQL/migrations/1649777528299-CreateClient.ts
+++ src/infra/database/postgresSQL/migrations/1649777528299-CreateClient.ts
@@ -21,7 +21,7 @@ export class CreateClient1649777528299 implements MigrationInterface {
           },
           {
             name: 'birthdate',
-            type: 'string',
+            type: 'varchar',
           },
           {
             name: 'age',
