c0d4bf908af68d397353e4e851621673d7044bfc
Caroll <carolinne.silva888@gmail.com>
2022-04-19 11:40:37 -0300

    ref: add try and catch

--- src/app/controllers/CityController.ts
+++ src/app/controllers/CityController.ts
@@ -1,26 +1,23 @@
 import { Request, Response } from 'express';
-// eslint-disable-next-line import/extensions
 import { CityService } from '../service/CityService';
 
 export class CityController {
   async createCity(req: Request, res: Response) {
-    const service = new CityService();
-    const result = await service.create(req.body);
-    if (result instanceof Error) {
-      return res.status(400).json(result.message);
-    }
-    return res.json(result);
+    try {
+      const service = new CityService();
+      const result = await service.create(req.body);
+      return res.status(200).json(result);
+    } catch (error) { return res.status(400).json({ 'description:': error.message, 'name:': error.name }); }
   }
 
   async findCity(req: Request, res: Response) {
-    const state = req.query.state as string;
-    const name = req.query.name as string;
-    const service = new CityService();
+    try {
+      const state = req.query.state as string;
+      const name = req.query.name as string;
+      const service = new CityService();
 
-    const result = await service.find({ name, state });
-    if (result instanceof Error) {
-      return res.status(400).json(result.message);
-    }
-    return res.status(200).json(result);
+      const result = await service.find({ name, state });
+      return res.status(200).json(result);
+    } catch (error) { return res.status(400).json({ 'description:': error.message, 'name:': error.name }); }
   }
 }
--- src/app/controllers/ClientController.ts
+++ src/app/controllers/ClientController.ts
@@ -1,64 +1,57 @@
-/* eslint-disable object-curly-newline */
 import { Request, Response } from 'express';
-// eslint-disable-next-line import/extensions
 import { ClientService } from '../service/ClientService';
 
 export class ClientController {
   async createClient(req: Request, res: Response) {
-    const service = new ClientService();
-    const result = await service.create(req.body);
-    if (result instanceof Error) {
-      return res.json(result.message);
-    }
-    return res.json(result);
+    try {
+      const service = new ClientService();
+      const result = await service.create(req.body);
+      return res.status(200).json(result);
+    } catch (error) { return res.status(400).json({ 'description:': error.message, 'name:': error.name }); }
   }
 
   async findClient(req: Request, res: Response) {
-    // const { fullname, gender, birthdate, age, city } = req.query;
+    try {
+      const fullname = req.query.fullname as string;
+      const gender = req.query.gender as string;
+      const birthdate = req.query.birthdate as string;
+      const age = req.query.age as any;
+      const city = req.query.city as string;
 
-    const fullname = req.query.fullname as string;
-    const gender = req.query.gender as string;
-    const birthdate = req.query.birthdate as string;
-    const age = req.query.age as any;
-    const city = req.query.city as string;
+      const service = new ClientService();
 
-    const service = new ClientService();
-
-    const result = await service.find({ fullname, gender, birthdate, age, city });
-    if (result instanceof Error) {
-      return res.status(400).json(result.message);
-    }
-    return res.status(200).json(result);
+      const result = await service.find({
+        fullname, gender, birthdate, age, city,
+      });
+      return res.status(200).json(result);
+    } catch (error) { return res.status(400).json({ 'description:': error.message, 'name:': error.name }); }
   }
 
   async findOneClient(req: Request, res: Response) {
-    const service = new ClientService();
-    const { id } = req.params;
-    const result = await service.findOne(id);
-    if (result instanceof Error) {
-      return res.status(400).json(result.message);
-    }
-    return res.status(200).json(result);
+    try {
+      const service = new ClientService();
+      const { id } = req.params;
+      const result = await service.findOne(id);
+      return res.status(200).json(result);
+    } catch (error) { return res.status(400).json({ 'description:': error.message, 'name:': error.name }); }
   }
 
   async deleteClient(req: Request, res: Response) {
-    const service = new ClientService();
-    const { id } = req.params;
-    const result = await service.delete(id);
-    if (result instanceof Error) {
-      return res.status(400).json(result.message);
-    }
-    return res.status(204).end();
+    try {
+      const service = new ClientService();
+      const { id } = req.params;
+      await service.delete(id);
+      return res.status(204).end();
+    } catch (error) { return res.status(400).json({ 'description:': error.message, 'name:': error.name }); }
   }
 
   async updateClient(req: Request, res: Response) {
-    const { id } = req.params;
-    const { fullname } = req.body;
-    const service = new ClientService();
-    const result = await service.update({ id, fullname });
-    if (result instanceof Error) {
-      return res.status(400).json(result.message);
-    }
-    return res.status(200).json(result);
+    try {
+      const { id } = req.params;
+      const { fullname } = req.body;
+      const service = new ClientService();
+      const result = await service.update({ id, fullname });
+      return res.status(200).json(result);
+    } catch (error) { return res.status(400).json({ 'description:': error.message, 'name:': error.name }); }
   }
 }
